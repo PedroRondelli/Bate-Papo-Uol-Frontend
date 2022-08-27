02af3f9e4ea6e0cb9fcca9009a77435d6d03f80a
Pedro Rondelli <pedrorondelli@hotmail.com>
2022-08-27 18:42:41 -0300

    Carregamento mensagens

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
-let nomeUsuario;
-function perguntarNome() {  
-    let acesso ="não"
-    while(acesso==="não"){  
-        nomeUsuario = prompt("Qual é o seu lindo nome?")
-        const objeto = {name : nomeUsuario}
-        if( nomeUsuario!= null && nomeUsuario.length > 2){
-            axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', objeto )
-            acesso="sim"  
-        }
-        else{
-            alert("Nome inválido, digite outro")
-            acesso="não"
-        }
-    }    
-}   
+function perguntarNome() {
+    nomeUsuario = prompt("Nome inválido.Digite outro nome")
+    objeto = { name : nomeUsuario }
+    promessaPost = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", objeto)
+    promessaPost.then(sucessoNomeUsuario)
+    promessaPost.catch(erroNomeUsuario)
+    
+}
 
-perguntarNome()
+function erroNomeUsuario() {
+    console.log("Esse nome não é válido")
+    perguntarNome()
+}
+function sucessoNomeUsuario() {
+    console.log("Esse nome é válido")
+    entrouChat()
+
+}
+function entrouChat() {
+    const promessaGetMensagens = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
+    promessaGetMensagens.then(renderizarMensagem)
+    promessaGetMensagens.catch(entrouChat)
+}
+
+function renderizarMensagem(resposta) {
+    const arrayMensagens = resposta.data
+    for(let i=0 ; i < arrayMensagens.length ; i++){
+        const chat  = document.querySelector(".chat")
+        let tipo = arrayMensagens[i].type
+        verificar(tipo,arrayMensagens[i],chat)
+    }
+    
+
+    
+}
+function verificar(modeloMsg,objetoUsuario,sala) {
+    if(modeloMsg ==='status'){
+        sala.innerHTML = sala.innerHTML + `<div class=" statusbox boxChat"><span class="timer">(${objetoUsuario.time})</span> <span class="negrito">${objetoUsuario.from}</span> <span class="texto">${objetoUsuario.text}</span></div>`
+    }else if(modeloMsg ==='private_message'){
+         
+    }else if(modeloMsg === 'message'){
+         
+    }
+}
+alert("Olá, que bom ter você por aqui !")
+let nomeUsuario = prompt(" Qual é o seu nome de usuário ?" )
+let objeto = { name : nomeUsuario }
+let promessaPost = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", objeto) 
+promessaPost.then(sucessoNomeUsuario)
+promessaPost.catch(erroNomeUsuario)
+
