cfe8bbc23e2d80b6bf77515e2ca9ff34f1f7d018
Pedro Rondelli <pedrorondelli@hotmail.com>
2022-08-29 14:00:47 -0300

    Chat atualiza e move até o ultimo elemento

--- script.js
+++ script.js
@@ -1,79 +1,120 @@
 function perguntarNome() {
-    nomeUsuario = prompt("Nome inválido.Digite outro nome")
-    objeto = { name : nomeUsuario }
-    promessaPost = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", objeto)
-    promessaPost.then(sucessoNomeUsuario)
-    promessaPost.catch(erroNomeUsuario)
-    
-}
+    nomeDigitado  = prompt("Qual o seu nome?")
+    nomeChecado = {name:nomeDigitado}
+    promessaChecagemNome = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nomeChecado)
+    promessaChecagemNome.then(mantemConectadoeRendenizaMsg)
+    promessaChecagemNome.catch(perguntarNovamente)
+}    
 
-function erroNomeUsuario() {
-    console.log("Esse nome não é válido")
+ function mantemConectadoeRendenizaMsg() {
+    setInterval(bateponto, 5000)
+    RequisicaoArraydeMsgs()
+ }
+ function perguntarNovamente() {
     perguntarNome()
-}
-function sucessoNomeUsuario() {
-    console.log("Esse nome é válido")
-    entrouChat()
+ }
+ function bateponto() {
+    const promessaConectado = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nomeChecado )
+    promessaConectado.then(estouaqui)
+    promessaConectado.catch(desconectei)
+ }
+ function estouaqui() {
+    console.log('...')
+ }
+ function desconectei() {
+    console.log('Cai!!')
+ }
+
+let nomeDigitado  = prompt("Qual o seu nome?")
+let nomeChecado = {name:nomeDigitado}
+let promessaChecagemNome = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nomeChecado)
+promessaChecagemNome.then(mantemConectadoeRendenizaMsg)
+promessaChecagemNome.catch(perguntarNovamente)
+let chat = document.querySelector(".chat")
+let ultimamsg;
+let arrayComparador;
+let indexdoultimo;
+
+
+
+
+function RequisicaoArraydeMsgs() {
+    let promessaCarregarmsg = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
+    promessaCarregarmsg.then(PrimeirasMsgs)
+    promessaCarregarmsg.catch(RequisicaoArraydeMsgs)
 
 }
-function entrouChat() {
-    const promessaGetMensagens = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
-    promessaGetMensagens.then(renderizarMensagem)
-    promessaGetMensagens.catch(entrouChat)
+function PrimeirasMsgs(resposta) {
+    ultimamsg = resposta.data[99]
+    arrayComparador = resposta.data
+    resposta.data.forEach(verificacaoTipo)
+    setInterval(RequisicaoChat, 3000)
 }
+function verificacaoTipo(elemento) {
 
-function renderizarMensagem(resposta) {
-    const arrayMensagens = resposta.data
-    for(let i=0 ; i < arrayMensagens.length ; i++){
-        const chat  = document.querySelector(".chat")
-        let tipo = arrayMensagens[i].type
-        verificar(tipo,arrayMensagens[i],chat)
+    let tipo = elemento.type
+    if(tipo === "status"){
+        chat.innerHTML = chat.innerHTML +`<div class="boxChat statusbox escopoStatus "><span class="timer">(${elemento.time})</span> <span class="negrito">${elemento.from}</span> ${elemento.text}</div>`
+        let esseDeveSerVisto = document.querySelector(".escopoStatus")
+        esseDeveSerVisto.scrollIntoView()
+        console.log(esseDeveSerVisto.classList)
+        esseDeveSerVisto.classList.remove("escopoStatus")
     }
-    arrayComparacao.push(arrayMensagens)
-    setInterval(chamada, 5000)
-    setInterval(atualizarChat, 3000)
-
-    
-}
-function verificar(modeloMsg,objetoUsuario,sala) {
-    if(modeloMsg ==='status'){
-        sala.innerHTML = sala.innerHTML + `<div class=" statusbox boxChat"><span class="timer">(${objetoUsuario.time})</span> <span class="negrito">${objetoUsuario.from}</span> <span class="texto">${objetoUsuario.text}</span></div>`
-    }else if(modeloMsg ==='private_message'){
-         
-    }else if(modeloMsg === 'message'){
-         
-    }
-}
-function chamada() {
-    const promessaChamada = axios.post("https://mock-api.driven.com.br/api/v6/uol/status",objeto)
-    promessaChamada.then(estouConectado)
-    promessaChamada.catch()
-}
-function estouConectado() {
-    console.log(" Estou conectado!!! ")
-}
-function atualizarChat() {
-    const promessaAtualizar = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
-    promessaAtualizar.then(comparaArrays)
-    promessaAtualizar.catch()
-    
-
-}
-function comparaArrays(resposta){
-   let arrayNovo  = resposta.data
-   for(i = 99;i < 0; i-1){
-    if(arrayNovo[i]!= ultimaMsg){
+    else if(tipo === "message"){
+        chat.innerHTML = chat.innerHTML +`<div class="boxChat messagebox escopoMessage"><span class="timer">(${elemento.time})</span> <span class="negrito">${elemento.from}</span> para <span class="negrito">${elemento.to}</span>: ${elemento.text}</div>`
+        let esseDeveSerVisto = document.querySelector(".escopoMessage")
+        esseDeveSerVisto.scrollIntoView()
+        console.log(esseDeveSerVisto.classList)
+        esseDeveSerVisto.classList.remove("escopoMessage")
         
     }
-       
-   }
-   
+    else if(tipo === "private_message"){
+        chat.innerHTML = chat.innerHTML + `<div class="boxChat privatebox escopoPrivate"><span class="timer">(${elemento.time})</span> <span class="negrito">${elemento.from}</span> reservadamente para <span class="negrito">${elemento.to}</span>: ${elemento.text}  `
+        let esseDeveSerVisto = document.querySelector(".escopoPrivate")
+        esseDeveSerVisto.scrollIntoView()
+        console.log(esseDeveSerVisto.classList)
+        esseDeveSerVisto.classList.remove("escopoPrivate")
+        
+    }
 }
-let arrayComparacao;
-alert("Olá, que bom ter você por aqui !")
-let nomeUsuario = prompt(" Qual é o seu nome de usuário ?" )
-let objeto = { name : nomeUsuario }
-let promessaPost = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", objeto) 
-promessaPost.then(sucessoNomeUsuario)
-promessaPost.catch(erroNomeUsuario)
+function RequisicaoChat() {
+    let promessaAtualizarmsgs = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
+    promessaAtualizarmsgs.then(atualizaChat)
+    promessaAtualizarmsgs.catch()
+}
+function atualizaChat(resposta) {
+    let arrayNovo = resposta.data
+    arrayNovo.forEach(objetoNovo => {
+        if(JSON.stringify(objetoNovo) === JSON.stringify(ultimamsg)){
+            indexdoultimo = arrayNovo.indexOf(objetoNovo)
+            for(let e=indexdoultimo + 1;e < 100;e++){
+            verificacaoTipo(arrayNovo[e])
+            }
+        }
+        
+    });
+    arrayComparador = arrayNovo
+    indexdoultimo = 0
+    ultimamsg = arrayNovo[99]
 
+    
+}
+
+let teste1=[{name:"tevos"},{name:"hermat"},{name:"pimba"},{name:"chachac"}]
+// let teste2=[{name:"pimba"},{name:"chachac"},{name:"mao"},{name:"vacilao"}]
+// ultimamsg= teste1[3]
+
+// let indexMarcador;
+// teste2.forEach(checaseevelho)
+
+// for(let e=indexMarcador + 1;e < 4;e++){
+//     console.log(teste2[e])
+    // verificacaoTipo(teste2[e])
+// }
+
+// function checaseevelho(elemento) {
+//     if(JSON.stringify(ultimamsg) === JSON.stringify(elemento) ){
+//        let indexdoultimo = teste2.indexOf(elemento)
+//        indexMarcador = indexdoultimo
+//     }
+// }
